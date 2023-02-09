24259453ae84b7b6334c21eac69a0493e884531c
Gary Gonzalez <garyavalosc@gmail.com>
2023-02-08 23:50:19 -0600

    Adding connection to mysql

--- car_sales_pipeline.py
+++ car_sales_pipeline.py
@@ -5,8 +5,31 @@ import mysql.connector              # this module helps in connecting to the DB
 
 
 # DB Connection
-# Retrieve the values of the environment variables
-# Connect to the database
+def connect_to_database():
+    try:
+        # Retrieve the values of the environment variables
+        host = os.environ['DB_HOST']
+        user = os.environ['DB_USER']
+        password = os.environ['DB_PASSWORD']
+        database = os.environ['DB_NAME']
+        
+        connection = mysql.connector.connect(
+            host=host,
+            user=user,
+            password=password,
+            database=database
+        )
+        
+        print("Successfully connected to the database.")
+        return connection
+    except KeyError as e:
+        print(f"Error: Missing environment variable {e}")
+        return None
+    except mysql.connector.Error as e:
+        print(f"Error connecting to the database: {e}")
+        return None
+
+
 # Extract
 # CSV Extract Function
 # create an empty data frame to hold extracted data
