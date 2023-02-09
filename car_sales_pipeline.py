3da205a0ac733de7f926045858a85f034ea56c52
Gary Gonzalez <garyavalosc@gmail.com>
2023-02-08 23:40:08 -0600

    Steps for pipeline and importing primary modules

---
+++ car_sales_pipeline.py
@@ -0,0 +1,16 @@
+import glob                         # this module helps in selecting files 
+import pandas as pd                 # this module helps in processing CSV files
+import os                           # this module helps in accessing the env. variables
+import mysql.connector              # this module helps in connecting to the DB  
+
+
+# DB Connection
+# Retrieve the values of the environment variables
+# Connect to the database
+# Extract
+# CSV Extract Function
+# create an empty data frame to hold extracted data
+# Transform
+# Loading
+# Logging
+# Running ETL Process
